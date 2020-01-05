927219a24b456a142616b445e5cffffaf9bb63ed
shinsm0606 <shinsm0606@gamil.com>
2020-01-05 15:57:13 +0900

    test

--- src/main/java/com/a2b/trip/notice/model/service/NoticeService.java
+++ src/main/java/com/a2b/trip/notice/model/service/NoticeService.java
@@ -1,5 +1,15 @@
 package com.a2b.trip.notice.model.service;
 
+import java.util.ArrayList;
+
+import com.a2b.trip.notice.model.vo.Notice;
+
 public interface NoticeService {
+
+	ArrayList<Notice> selectListAllNotice();
+
+	Notice selectDetailViewNotice(int notice_no);
+
+	
 
 }
